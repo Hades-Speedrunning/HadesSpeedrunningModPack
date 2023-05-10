9cc7cdbe0b17428e0eaf61127c2738b880cb1971
SleepSoul <spider.coopplay@gmail.com>
2023-05-10 23:10:11 +0100

    fix(EnemyControl): Update EncounterData in UpdatePools
    Fix for #8 didn't update the EnemySets in EncounterData, behaviour when changing settings ingame should be far more reliable now. Also fixes some minor bugs around c1.

---
+++ EnemyControl/ReferenceData.lua
@@ -0,0 +1,45 @@
+EnemyControl.EncounterEnemySets = {
+    Generated = "EnemiesBiome1",
+    GeneratedTartarus = "EnemiesBiome1",
+    DevotionTestTartarus = "EnemiesBiome1Devotion",
+    ThanatosTartarus = "EnemiesBiome1Thanatos",
+    ShrineChallengeTartarus = "ShrineChallengeTartarus",
+    SurvivalTartarus = "EnemiesBiome1Survival",
+    MiniBossWretchAssassin = "EnemiesBiome1MiniBoss",
+    MiniBossSwarmerHelemted = "EnemiesBiome1MiniBoss",
+    MiniBossGrenadier = "EnemiesBiome1MiniBoss",
+    MiniBossSelfDestruct = "EnemiesBiome1MiniBoss",
+    MiniBossPitcher = "EnemiesBiome1MiniBoss",
+    MiniBossWaveFist = "EnemiesBiome1MiniBoss",
+
+    GeneratedAsphodel = "EnemiesBiome2",
+    TimeChallengeAsphodel = "EnemiesBiome2Challenge",
+    DevotionTestAsphodel = "EnemiesBiome2Devotion",
+    ThanatosAsphodel = "EnemiesBiome2Thanatos",
+    ShrineChallengeAsphodel = "ShrineChallengeAsphodel",
+    SurvivalAsphodel = "EnemiesBiome2Survival",
+    WrappingAsphodel = "EnemiesBiome2Wrapping",
+
+    GeneratedElysium = "EnemiesBiome3",
+    TimeChallengeElysium = "EnemiesBiome3",
+    DevotionTestElysium = "EnemiesBiome3Devotion",
+    ShrineChallengeElysium = "ShrineChallengeElysium",
+    SurvivalElysium = "EnemiesBiome3Survival",
+    MiniBossNakedSpawners = "EnemiesBiome3Hard",
+    MiniBossShadeMagic = "EnemiesBiome3Hard",
+
+    GeneratedStyx = "EnemiesBiome4",
+    GeneratedStyxMini = "EnemiesBiome4Mini",
+    GeneratedStyxMiniSingle = "EnemiesBiome4MiniSingle",
+    TimeChallengeStyx = "EnemiesBiome4",
+
+    BaseStyxMiniboss = "EnemiesBiome4MiniBossFodder",
+}
+
+EnemyControl.EncounterHardEnemySets = {
+    GeneratedTartarus = "EnemiesBiome1Hard",
+    GeneratedAsphodel = "EnemiesBiome2Hard",
+    GeneratedElysium = "EnemiesBiome3Hard",
+    GeneratedStyxMini = "EnemiesBiome4MiniHard",
+    GeneratedStyxMiniSingle = "EnemiesBiome4MiniSingle",
+}
